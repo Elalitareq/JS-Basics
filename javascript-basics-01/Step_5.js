b3f5ddeb1246f67d939955161dea213a2fba7518
tareqelali <elalitareq@gmail.com>
2022-12-07 16:00:49 +0200

    Step_5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,5 @@
+document.getElementById("validate").onclick=function(){
+num_1=document.getElementById("first_number").value;
+num_2=document.getElementById("second_number").value;
+alert(num_1 +"X"+ num_2 +"=" +num_1*num_2)
+}
