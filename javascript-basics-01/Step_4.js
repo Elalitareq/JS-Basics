723bdf3f88b2d40713e859a7298461467bee2606
tareqelali <elalitareq@gmail.com>
2022-12-07 15:52:02 +0200

    Step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,6 @@
+document.getElementById("validate").onclick = function(){
+name=document.getElementById("name").value;
+surname=document.getElementById("surname").value;
+city=document.getElementById("city").value;
+alert("Name: "+name+"\nSurname: "+surname+"\nCity: "+ city);
+}
