e55e74db4f031acda867609ce971f70b5360c4d5
tareqelali <elalitareq@gmail.com>
2022-12-08 21:08:39 +0200

    Reached create language array function

--- javascript-basics-04-tdd/questions.js
+++ javascript-basics-04-tdd/questions.js
@@ -1,101 +1,150 @@
 
 const stringSize = (text) => {
-
+    
+    a=text.length
+    return(a)
 }
 const replaceCharacterE = (text) => {
-
+    b=text.replace("e"," ")
+    return(b)
 }
 const concatString = (text1, text2) => {
-
+    c=text1+text2;
+    return(c)
 }
 const showChar5 = (text) => {
-
+    d=text.split("")[4]
+    return(d)
 }
 const showChar9 = (text) => {
-
+    e=text.slice(0,9)
+    return(e)
 }
 const toCapitals = (text) => {
-
+    f=text.toUpperCase()
+    return(f)
 }
 const toLowerCase = (text) => {
-
+    g=text.toLowerCase()
+    return(g)
 }
 const removeSpaces = (text) => {
-
+    h=text.slice(1,-1)
+    return(h)
 }
 const IsString = (text) => {
-
-}
+    if(typeof text==="string"){
+    return(true)}
+}  
 
 const getExtension = (text) => {
-
+    i=text.split(".").pop()
+    return(i)
 }
 const countSpaces = (text) => {
-
+    j=text.split(" ").length-1
+    return(j)
 }
 const InverseString = (text) => {
-
+    k=text.split("").reverse().join("")
+    return(k)
 }
 
 const power = (x, y) => {
-
+    l=x
+    for(i=1;i<y;i++){
+        l*=x
+    }
+    return(l)
 }
 const absoluteValue = (num) => {
-
+    if(num<0)(
+        num=0-num
+    )
+    return(num)
 }
 const absoluteValueArray = (array) => {
-
+    let x=[]
+    array.forEach(m => {
+     if(m<0){
+        m=-m
+     }   
+     x.push(m)
+    });
+    return(x)
 }
 const circleSurface = (radius) => {
-
+    area=3.14*radius*radius
+    g=Math.round(area)
+    return(g)
 }
 const hypothenuse = (ab, ac) => {
-
+    o=ab*ab+ac*ac
+    bc=Math.sqrt(o)
+    return(bc)
 }
 const BMI = (weight, height) => {
-
+    q=weight/(height*height)
+    return(Math.round(q*100)/100)
 }
 
 const createLanguagesArray = () => {
-
+    anArray=["Html","CSS","Java","PHP"]
+    return(anArray)
 }
 
 const createNumbersArray = () => {
-
+    anArray=[]
+    for(i=0;i<6;i++)
+    anArray.push(i)
+    return(anArray)
 }
 
 const replaceElement = (languages) => {
-
+    languages[2]="Javascript"
+    return(languages)
 }
 
 const addElement = (languages) => {
-
+    languages.push("Ruby","Python")
+    return(languages)
 }
 
 const addNumberElement = (numbers) => {
+    numbers.splice(0,0, -2,-1)
+    return(numbers)
 
 }
 
 const removeFirst = (languages) => {
-
+    languages.splice(0,1,)
+    return(languages)
 }
 
 const removeLast = (languages) => {
-
+    languages.splice(-1,1,)
+    return(languages)
 }
 
 const convertStrToArr = (social_arr) => {
-
+    l_array=[]
+    social_arr.split(",").forEach(g => {
+        l_array.push(g)
+    });
+    return(l_array)
 }
 
 const convertArrToStr = (languages) => {
-
+    f=languages.toString()
+    return(f)
 }
 
 const sortArr = (social_arr) => {
-
+    g=social_arr.sort()
+    return(g)
 }
 
 const invertArr = (social_arr) => {
-
+    g=social_arr.reverse()
+    return(g)
 }
