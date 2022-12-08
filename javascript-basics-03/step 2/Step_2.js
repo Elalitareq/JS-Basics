5d0c5b2fa8a5135f05cab23fc85a3dae7a4fda18
tareqelali <elalitareq@gmail.com>
2022-12-08 12:29:41 +0200

    Step_2

---
+++ javascript-basics-03/step 2/Step_2.js
@@ -0,0 +1,6 @@
+document.getElementById("hide").onclick=()=>{
+    document.getElementById("texte").style="color:transparent;"
+}
+document.getElementById("show").onclick=()=>{
+    document.getElementById("texte").style="color:black;"
+}
