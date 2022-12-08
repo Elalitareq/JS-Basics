817a941b817c903f1b0a767329b900daa0bf8a64
tareqelali <elalitareq@gmail.com>
2022-12-08 12:11:28 +0200

    instructions are vague i needed to redo step 3 because of that

--- javascript-basics-02/step 3/Step_3.js
+++ javascript-basics-02/step 3/Step_3.js
@@ -1,3 +1,4 @@
+document.querySelector("body").appendChild(document.createElement("div"))
 document.getElementById("name").onkeyup=()=>{
 i_text=document.getElementById("name").value;
 document.querySelector("div").innerHTML=i_text
