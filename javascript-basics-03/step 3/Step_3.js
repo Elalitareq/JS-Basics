2fcf8d9db2ea3835c16551887322e6450d05e9a2
tareqelali <elalitareq@gmail.com>
2022-12-08 14:56:25 +0200

    edited JS-B-3 testing another method to use one function for all

--- javascript-basics-03/step 3/Step_3.js
+++ javascript-basics-03/step 3/Step_3.js
@@ -7,3 +7,18 @@ red=document.querySelector(".green").onclick=()=>{
 red=document.querySelector(".blue").onclick=()=>{
     document.getElementById("text").style="color:blue;"
 }
+// or
+// g=document.querySelectorAll("div .color").forEach(c => {
+//     c.onclick=(p)=>{
+//         if(p.target.className=="color red"){
+//             document.getElementById("text").style="color:red;"
+//         }
+//         else if(p.target.className=="color green"){
+//             document.getElementById("text").style="color:green;"
+//         }
+//         else if(p.target.className=="color blue"){
+//             document.getElementById("text").style="color:blue;"
+//         }
+//     }
+
+// });
